102ef61cc0334477e4176b14035d9c74ee2f7b07
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-09-06 00:41:53 +0600

    Switch added on App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Appbar from "./AppBar/AppBar";
+import NotFound from "./Components/Warning/NotFound";
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Appbar></Appbar>
       
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
     </>
   );
 }
