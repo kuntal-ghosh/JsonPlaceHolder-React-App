cb18b3b157c8a8938f97dd8e567df06841cc6848
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-09-06 00:57:32 +0600

    useState and useEffect is added in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Appbar from "./AppBar/AppBar";
 import NotFound from "./Components/Warning/NotFound";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => setPosts);
   return (
     <>
       <Appbar></Appbar>
-      
+
       <Switch>
         <Route exact path="/"></Route>
         <Route path="/*">
