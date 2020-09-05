c36563c50c7a25d124a30a65348aca44171a0eeb
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-09-06 00:16:30 +0600

    material ui component added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Appbar from "./AppBar/AppBar";
+
+function App() {
+  return (
+    <>
+      <Appbar></Appbar>
+    </>
+  );
+}
+
+export default App;
