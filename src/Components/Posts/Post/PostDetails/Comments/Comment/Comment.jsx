2d23175ea9048f95c167f7516a6bb626d0581245
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-09-06 18:39:11 +0600

    comments dhown in card

--- src/Components/Posts/Post/PostDetails/Comments/Comment/Comment.jsx
+++ src/Components/Posts/Post/PostDetails/Comments/Comment/Comment.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    maxWidth: 700,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignItems: "center",
+    margin: "0 auto",
+    marginBottom: "20px",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const Comment = (props) => {
-    return ( 
-        <div>comment</div>
-     );
-}
- 
+  const { name } = props.comment;
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
 export default Comment;
--- src/Components/Posts/Post/PostDetails/Comments/Comments.jsx
+++ src/Components/Posts/Post/PostDetails/Comments/Comments.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Comment from "./Comment/Comment";
+import Typography from "@material-ui/core/Typography";
 
 const Comments = (props) => {
   const comments = props.comments;
   return (
     <>
+      <Typography
+        variant="h6"
+        style={{ textAlign: "center", margin: "50px 0" }}
+      >
+        Comments({comments.length})
+      </Typography>
       {comments.map((comment) => (
-        <Comment></Comment>
+        <Comment comment={comment} key={comment.id}></Comment>
       ))}
     </>
   );
